d172490f6565d7ff25b2ff69e29fd41657e680e7
Ed-hacker <kjskhw1217@naver.com>
2026-01-04 22:57:50 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 280 ms, Memory: 19272 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for(int i = 0; i < N; i++){ //i를 공백개수
+            for(int j = 0; j < i;  j++){
+                System.out.print(" ");
+            }
+            for(int k = 0; k < N-i; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+            }
+    }
+
+
+
+
