a9d7746341025ffc5e8e61a216eb16bc1b5df38c
Ed-hacker <kjskhw1217@naver.com>
2026-01-05 17:04:09 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 712 ms, Memory: 60636 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
+
+class Main {
+    public static class Point{
+        int x;
+        int y;
+
+        Point(int x, int y){
+           this.x = x;
+           this.y = y;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        Point [] arr = new Point[N]; //길이만큼 배열 만들어
+
+        for(int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a =Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr[i] = new Point(a,b);
+        } //일단 다 넣어
+
+        StringBuilder sb = new StringBuilder();
+
+        Arrays.stream(arr)
+                .sorted(Comparator.comparingInt((Point p) -> p.y)
+                        .thenComparingInt((Point p)-> p.x))
+                .forEach(p -> {
+                    sb.append(p.x).append(" ").append(p.y).append("\n");
+                });
+
+        System.out.println(sb);
+            }
+}
+
+
