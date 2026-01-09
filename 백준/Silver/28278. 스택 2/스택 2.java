56b202b9b315bd1d899b72c304def836730e8621
Ed-hacker <kjskhw1217@naver.com>
2026-01-09 12:22:23 +0900

    [Silver IV] Title: 스택 2, Time: 852 ms, Memory: 246004 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.java
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -11,25 +11,24 @@ class Main {
         Stack<Integer> stack = new Stack<>();
 
         int N = Integer.parseInt(br.readLine());//명령 갯수
+
+
         for(int i = 0; i < N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
-            if(a==1){
-                stack.push(Integer.parseInt(st.nextToken()));
+            switch(a) {
+                case 1: stack.push(Integer.parseInt(st.nextToken()));
+                        break;
+                case 2: sb.append(stack.empty() ? -1 : stack.pop()).append("\n");
+                        break;
+                case 3: sb.append(stack.size()).append("\n");
+                        break;
+                case 4: sb.append(stack.empty() ? 1 : 0 ).append("\n");
+                        break;
+                case 5: sb.append(stack.empty() ? -1 : stack.peek()).append("\n");
+                        break;
             }
-            else if(a==2){
-                if(stack.empty())sb.append(-1).append("\n");
-                else sb.append(stack.pop()).append("\n");
             }
-            else if(a==3)sb.append(stack.size()).append("\n");
-            else if(a==4){if(stack.empty())sb.append(1).append("\n");
-                    else sb.append(0).append("\n");
-            }
-            else if(a==5) {if(stack.empty())sb.append(-1).append("\n");
-            else sb.append(stack.peek()).append("\n");
-        }}
-
-
 
         System.out.println(sb);
 
@@ -37,6 +36,7 @@ class Main {
 
         }
 }
+
 
 
 
