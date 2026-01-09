68a5fb9d098940137dc6b0abbea25426c422ee8f
Ed-hacker <kjskhw1217@naver.com>
2026-01-09 11:44:34 +0900

    [Bronze I] Title: 최소공배수, Time: 120 ms, Memory: 14548 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -3,28 +3,9 @@ import java.util.*;
 
 
 class Main {
-    public static int calculate(int a, int b){
-        int min = Math.min(a,b);//작은거 6
-        int max = Math.max(a,b);//큰 거 10
-        int result = 1;
-        boolean end = false;
-
-        int c = min;  //6
-
-        while(!end) {
-            end = true;
-            for (int i = c; i >= 2; i--) { //큰거부터이
-                if(min%i==0 && max%i ==0){
-                    min /= i;//3
-                    max /= i;//5
-                    result *= i;//2
-                    end = false; //나누어졋음
-                }
-            }
-
-        }
-        result = result * min * max;
-        return result;
+    public static int gdc(int a, int b){ //최대공약수 a가 큰거 b가 작은거
+        if(b==0)return a;
+        return gdc(b,a%b);
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -37,7 +18,9 @@ class Main {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            sb.append(calculate(a,b)).append("\n");
+            int max = Math.max(a,b);
+            int min = Math.min(a,b);
+            sb.append(a*b/gdc(max,min)).append("\n");
         }
 
 
@@ -47,7 +30,3 @@ class Main {
 
         }
 }
-
-
-
-
