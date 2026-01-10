f7db3479778049c141f208d2b7787e7c49215c50
Ed-hacker <kjskhw1217@naver.com>
2026-01-10 23:36:12 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 64 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+        long N = Long.parseLong(br.readLine());
+
+
+        System.out.println(N*N);
+        System.out.print(2);
+
+
+    }
+
+
+}
