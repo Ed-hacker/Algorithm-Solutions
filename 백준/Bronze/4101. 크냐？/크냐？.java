6b2b0a8d4d142c42e484d956b0d086527e4a700b
Ed-hacker <kjskhw1217@naver.com>
2026-01-27 00:58:49 +0900

    [Bronze V] Title: 크냐?, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringBuilder sb = new StringBuilder();
+        while(true){
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        if(N==M && N==0)break;
+            else if( N>M){
+                sb.append("Yes").append("\n");
+            }
+            else sb.append("No").append("\n");
+        
+        }
+    
+        System.out.print(sb);
+
+    }
+}
