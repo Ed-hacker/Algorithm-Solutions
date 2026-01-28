f9ba439692fb1abb4b70b6da57e5fd97d90a33ed
Ed-hacker <kjskhw1217@naver.com>
2026-01-28 23:31:52 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 60 ms, Memory: 11256 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+class Main{
+    public static void main(String[] args){
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             System.out.println("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+
+    }
+}
