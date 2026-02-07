fc2fd243db7884c828949e227c05e7c10833981f
강현욱 <kjskhw1217@naver.com>
2026-02-07 22:07:38 +0900

    [Bronze V] Title: 웰컴, Time: 60 ms, Memory: 11220 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+
+    }
+}
