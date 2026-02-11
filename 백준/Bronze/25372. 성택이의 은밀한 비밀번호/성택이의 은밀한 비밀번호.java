77c532bcf29de319d81fc058da1cff2eb04446b0
강현욱 <kjskhw1217@naver.com>
2026-02-11 23:06:04 +0900

    [Bronze V] Title: 성택이의 은밀한 비밀번호, Time: 172 ms, Memory: 17480 KB -BaekjoonHub

---
+++ 백준/Bronze/25372. 성택이의 은밀한 비밀번호/성택이의 은밀한 비밀번호.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.nextLine();
+        for(int i = 0; i < N ; i++){
+            String str = sc.nextLine();
+            if(str.length()>=6 && str.length()<=9){
+                System.out.println("yes");
+            }else System.out.println("no");
+        }
+
+    }
+}
