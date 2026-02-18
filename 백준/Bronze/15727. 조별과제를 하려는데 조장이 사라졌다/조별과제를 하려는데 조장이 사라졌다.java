d1b7c0fcef3d5048cc5b62e3067e46dd743a9c60
강현욱 <kjskhw1217@naver.com>
2026-02-19 00:01:27 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 68 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        if(N%5 == 0) System.out.println(N/5);
+        else System.out.println(N/5+1);
+
+
+
+
+
+
+    }
+}
