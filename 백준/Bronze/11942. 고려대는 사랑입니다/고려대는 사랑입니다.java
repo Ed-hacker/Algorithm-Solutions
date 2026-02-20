a06827c8ebde6377f115868e7995008da650b541
강현욱 <kjskhw1217@naver.com>
2026-02-21 01:01:35 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 60 ms, Memory: 11464 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+            System.out.println("고려대학교");
+        }
+
+
+
+
+
+
+    }
