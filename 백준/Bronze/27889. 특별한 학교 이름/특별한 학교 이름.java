a4b40a105aaf63f870f7bdd6d783f80b91602304
강현욱 <kjskhw1217@naver.com>
2026-02-26 23:33:21 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 64 ms, Memory: 11396 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        if(a.equals("NLCS")) System.out.println("North London Collegiate School");
+        else if(a.equals("BHA")) System.out.println("Branksome Hall Asia");
+        else if(a.equals("KIS")) System.out.println("Korea International School");
+        else if(a.equals("SJA")) System.out.println("St. Johnsbury Academy");
+
+      
+
+
+
+    }
+
+
+
+
+
+
+    }
