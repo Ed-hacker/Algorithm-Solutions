3629b553cd8679205f6e7e332a75f710610ecef8
강현욱 <kjskhw1217@naver.com>
2026-03-01 01:14:36 +0900

    [Bronze IV] Title: 평균 점수, Time: 64 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            int a = Integer.parseInt(br.readLine());
+            if(a>=40)sum+=a;
+            else sum+= 40;
+        }
+        System.out.println(sum / 5);
+    }}
