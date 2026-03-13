3944f04b47db92100a3e1e24cbc317c1cc9c4d55
강현욱 <kjskhw1217@naver.com>
2026-03-13 12:17:08 +0900

    [Silver III] Title: 수열, Time: 208 ms, Memory: 22044 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -17,17 +17,16 @@ class Main {
             arr[i] = Integer.parseInt(st2.nextToken());
         }
 
-        int[] sumArr = new int[N-k+1];
-        for(int i = 0; i < N-k+1; i++){
-            int sum = 0;
-            for(int j = i; j < i+ k; j++){
-                sum += arr[j];
-            }
-            sumArr[i] = sum;
+        int[] sumArr = new int[N]; //누적합 배열
+        sumArr[0] = arr[0];
+        for(int i = 1; i < N; i++){
+             sumArr[i] = sumArr[i-1] + arr[i];
         }
         int result = Integer.MIN_VALUE;
-        for(int i : sumArr){
-            result = Math.max(result,i);
+        for(int i = 0; i < N-k+1; i++){
+            if(i >= 1){
+            result = Math.max(result, sumArr[i+k-1] - sumArr[i-1]);}
+            else result = Math.max(result, sumArr[k-1]);
         }
 
         System.out.println(result);
