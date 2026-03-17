0c576d95a7350ce29e2e7983a2ad088bdcce4806
강현욱 <kjskhw1217@naver.com>
2026-03-17 11:53:47 +0900

    [Silver II] Title: 종이의 개수, Time: 804 ms, Memory: 319764 KB -BaekjoonHub

---
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+    static int[][] arr;
+    static int countZero = 0;
+    static int countOne = 0;
+    static int countMinus = 0;
+
+    private static boolean check(int x, int y, int n){ //시작좌표, 체크할 정사각형 크기
+
+        for(int i = x; i < x+n; i++){
+            for(int j = y; j < y+n; j++){
+                if(arr[x][y] != arr[i][j])return false;
+            }
+        }
+        if(arr[x][y] == 0) countZero++;
+        else if(arr[x][y] == 1) countOne++;
+        else countMinus++;    //사각형 인정~
+
+        return true;
+    }
+
+    private static void divide(int x, int y, int n){
+
+        if(!check(x,y,n)){
+            divide(x,y,n/3);
+            divide(x, y + n/3 , n/3);
+            divide(x, y + 2*n/3, n/3);
+            divide(x + n/3,y,n/3);
+            divide(x + n/3, y + n/3 , n/3);
+            divide(x + n/3, y + 2*n/3, n/3);
+            divide(x+ 2*n/3,y,n/3);
+            divide(x+ 2*n/3, y + n/3 , n/3);
+            divide(x+ 2*n/3, y + 2*n/3, n/3);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N][N];
+
+        for(int i = 0; i < N; i ++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j++){
+                arr[i][j] =Integer.parseInt(st.nextToken());
+            }
+        }
+
+        divide(0,0,N);
+
+        System.out.println(countMinus);
+        System.out.println(countZero);
+        System.out.println(countOne);
+
+
+    }
+
+ }
