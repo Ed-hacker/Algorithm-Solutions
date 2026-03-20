85322bca8b459ff9f63b80cb16ea9ae63394d46e
강현욱 <kjskhw1217@naver.com>
2026-03-20 11:45:04 +0900

    [Silver IV] Title: 수 찾기, Time: 1112 ms, Memory: 52836 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+    static long[] arr;
+
+    private static boolean check(long target){
+        int low = 0;
+        int high = arr.length-1;
+
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+
+            if(arr[mid] == target) return true;
+            else if(arr[mid] < target) low = mid + 1;
+            else{
+                high = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        arr = new long[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        for(int i = 0; i < N; i++){
+            arr[i] = Long.parseLong(st.nextToken());
+        }
+        
+        Arrays.sort(arr); // 이분탐색 하기위해 정렬
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < M; i++){
+            if(check(Long.parseLong(st.nextToken()))) System.out.println(1);
+            else System.out.println(0);
+        }
+    }
+}
