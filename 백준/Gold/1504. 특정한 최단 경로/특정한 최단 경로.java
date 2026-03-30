9b67c32e0ff4f865ca3907eacfbf303e7c84d650
강현욱 <kjskhw1217@naver.com>
2026-03-30 14:58:50 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 532 ms, Memory: 68288 KB -BaekjoonHub

---
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -0,0 +1,101 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+    static class Node{
+        int to; int value;
+        Node( int to, int value){
+            this.to = to;
+            this.value = value;
+        }
+    }
+    static int V;
+    static ArrayList<Node>[] adj;
+    static PriorityQueue<Node> que;
+    static int[] dist;
+
+    static void bfs(int K){//시작정점
+        dist = new int[V+1];
+        for(int i =1; i <= V; i++){
+            dist[i] = Integer.MAX_VALUE;
+        }
+        dist[K] = 0;
+
+        que = new PriorityQueue<>((o1, o2) -> o1.value - o2.value);
+
+        que.add(new Node(K,0));
+
+        while(!que.isEmpty()){
+            Node now = que.poll();
+            int current_dist = now.value;
+
+            if(dist[now.to] < current_dist)continue; //이미 최단거리가 등록되어있으므로 continue(스킵)
+
+            for(Node next : adj[now.to]){
+                if(current_dist+next.value < dist[next.to]){
+                    dist[next.to] = current_dist+next.value;
+                    que.add(new Node(next.to, current_dist+next.value));
+                    }
+                }
+            }
+
+        }
+
+
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());//노드 갯수
+        int E= Integer.parseInt(st.nextToken()); //간선 갯수
+
+
+        adj = new ArrayList[V+1];
+        for(int i =1; i <= V; i++){
+            adj[i] = new ArrayList<>();
+        }
+
+        for(int i = 0; i < E; i++){
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
+
+
+
+            adj[from].add(new Node(to,value));
+            adj[to].add(new Node(from,value));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+
+        bfs(1);
+        int oneToa = dist[a];
+        int oneTob = dist[b];
+
+        bfs(a);
+        int aToN = dist[V];
+        int aTob = dist[b];
+
+        bfs(b);
+        int bToN = dist[V];
+
+
+
+        int shortest = Math.min(oneToa + bToN , oneTob + aToN) + aTob;
+
+
+        if((oneToa == Integer.MAX_VALUE || bToN == Integer.MAX_VALUE || aTob == Integer.MAX_VALUE) &&
+                (oneTob == Integer.MAX_VALUE || aToN == Integer.MAX_VALUE || aTob == Integer.MAX_VALUE)) System.out.println(-1);
+        else System.out.println(shortest);
+
+
+    }
+}
