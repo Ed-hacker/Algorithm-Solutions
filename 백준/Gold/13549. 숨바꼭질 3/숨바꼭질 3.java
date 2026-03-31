e50323f11fb46cd2b328f0111fcdc1fbfd45ba17
강현욱 <kjskhw1217@naver.com>
2026-03-31 14:18:54 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 132 ms, Memory: 18468 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -6,67 +6,59 @@ import java.util.*;
 class Main {
     private static boolean[] visited = new boolean[100001];
     private static int[] map = new int [100001];
-    private static Queue<Integer> que = new LinkedList<>();
+    private static Deque<Integer> que = new LinkedList<>();
 
 
-    private static int bfs(int N, int K){
+    private static int bfs(int N, int K) {
         visited[N] = true;
         que.add(N);
 
 
-        while(!que.isEmpty()){
+        while (!que.isEmpty()) {
             int now = que.poll();
-            if(now == K)return map[now];
+            if (now == K) return map[now];
 
-            if(now - 1 >= 0 && now -1 <= 100000){
-                if(!visited[now -1]){
-                    visited[now -1] = true;
-                    map[now -1] = map[now] + 1;
-                    que.add(now -1);
-                }else if(visited[now-1] && map[now-1] > map[now] + 1){
+            if (now * 2 >= 0 && now * 2 <= 100000) {
+                if (!visited[now * 2]) {
+                    visited[now * 2] = true;
+                    map[now * 2] = map[now];
+                    que.addFirst(now * 2);
+
+                }
+            }
+
+            if (now - 1 >= 0 && now - 1 <= 100000) {
+                if (!visited[now - 1]) {
+                    visited[now - 1] = true;
                     map[now - 1] = map[now] + 1;
-                    que.add(now -1);
+                    que.addLast(now - 1);
                 }
             }
 
-            if(now + 1 >= 0 && now + 1 <= 100000){
-                if(!visited[now +1]){
-                    visited[now +1] = true;
-                    map[now +1] = map[now] + 1;
-                    que.add(now +1);
-                }else if(visited[now+1] && map[now+1] > map[now] + 1){
-                    map[now +1] = map[now] + 1;
-                    que.add(now +1);
+                if (now + 1 >= 0 && now + 1 <= 100000) {
+                    if (!visited[now + 1]) {
+                        visited[now + 1] = true;
+                        map[now + 1] = map[now] + 1;
+                        que.addLast(now + 1);
+                    }
                 }
-            }
 
-            if(now*2 >= 0 && now*2 <= 100000){
-                if(!visited[now*2]){
-                    visited[now*2] = true;
-                    map[now*2] = map[now];
-                    que.add(now*2);
-                }else if(visited[now*2] && map[now*2] > map[now]){
-                    map[now*2] = map[now];
-                    que.add(now*2);
-                }
+                
+
             }
-        }
 
 
         return 0;
+
     }
+    public static void main (String[]args) throws IOException {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                    StringTokenizer st = new StringTokenizer(br.readLine());
+                    int N = Integer.parseInt(st.nextToken()); //수빈이가 있는 위치
+                    int K = Integer.parseInt(st.nextToken()); //동생이 있는 위치
 
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); //수빈이가 있는 위치
-        int K = Integer.parseInt(st.nextToken()); //동생이 있는 위치
-
-
-
-        System.out.println(bfs(N,K));
-    }
-}
-
+                    System.out.println(bfs(N, K));
+                }
+            }
 
