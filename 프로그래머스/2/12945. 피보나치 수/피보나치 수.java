eee91a84e99cb28b18ff8f3811b15b011b5b24a8
강현욱 <kjskhw1217@naver.com>
2026-07-02 11:38:10 +0900

    [level 2] Title: 피보나치 수, Time: 1.69 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,17 +1,13 @@
 class Solution {
-    public int dp(int[] arr, int t){
-        if(arr[t] == 0 && t > 1){
-        return arr[t] = (dp(arr,t-1) + dp(arr,t-2)) % 1234567;
-        }
-        else return arr[t];
-    }
-    
     public int solution(int n) {
         int[] arr = new int[n+1];
         arr[0] = 0;
         arr[1] = 1;
         
-        return dp(arr,n);
- 
+        for(int i = 2; i <= n; i++){
+            arr[i] = (arr[i-1] + arr[i-2]) % 1234567;
+        }
+        
+        return arr[n];
     }
 }
