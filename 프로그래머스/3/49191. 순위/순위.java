4883cd437653581d800561fa9cc62e52d84e4ee1
강현욱 <kjskhw1217@naver.com>
2026-08-28 15:28:00 +0900

    [level 3] Title: 순위, Time: 4.29 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int solution(int n, int[][] results) {
+        boolean[][] win = new boolean[n + 1][n + 1];
+
+
+        for (int[] res : results) {
+            win[res[0]][res[1]] = true; 
+        }
+
+        
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (win[i][k] && win[k][j]) {
+                        win[i][j] = true;
+                    }
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            int count = 0;
+            for (int j = 1; j <= n; j++) {
+                if (i == j) continue;
+                
+              
+                if (win[i][j] || win[j][i]) {
+                    count++;
+                }
+            }
+           
+            if (count == n - 1) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
