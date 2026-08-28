696b1182e6df36da55d40c6f097ad96a9ba4a399
강현욱 <kjskhw1217@naver.com>
2026-08-28 15:41:54 +0900

    [level 3] Title: 순위, Time: 5.72 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,38 +1,30 @@
 class Solution {
     public int solution(int n, int[][] results) {
-        boolean[][] win = new boolean[n + 1][n + 1];
+        boolean[][] win = new boolean[n+1][n+1];
 
-
-        for (int[] res : results) {
-            win[res[0]][res[1]] = true; 
+        for(int[] res : results ){
+            win[res[0]][res[1]] = true; //경기결과 기록
         }
 
-        
-        for (int k = 1; k <= n; k++) {
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= n; j++) {
-                    if (win[i][k] && win[k][j]) {
+        for(int k = 1; k <= n; k++){
+            for(int i = 1; i <= n; i++){
+                for(int j = 1; j <= n; j++){
+                    if(win[i][k] && win[k][j])
                         win[i][j] = true;
-                    }
                 }
             }
-        }
+        } //연속관계 기록
+
 
         int answer = 0;
-        for (int i = 1; i <= n; i++) {
-            int count = 0;
-            for (int j = 1; j <= n; j++) {
-                if (i == j) continue;
-                
-              
-                if (win[i][j] || win[j][i]) {
-                    count++;
-                }
+
+        for(int i = 1; i <=n; i++){//기준 선수
+            int cnt = 0;
+            for(int j = 1; j <=n; j++){
+                if(win[i][j] || win[j][i]) cnt++;
             }
-           
-            if (count == n - 1) {
-                answer++;
-            }
+
+            if(cnt == n -1 )answer++;
         }
 
         return answer;
